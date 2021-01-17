d62b88146fec182226b3e89152000b16ebcaecac
tomfrankkirk <tomfrankkirk@gmail.com>
2021-01-17 20:45:47 +0000

    trim paths

--- vaxitrack/urls.py
+++ vaxitrack/urls.py
@@ -4,14 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    # path('', views.userpage, name="userpage"),
     path('userpage/', views.userpage, name="userpage"),
-    # path("userpage", views.userpage, name="userpage"),
-    # path("userpage.html", views.userpage, name="userpage"),
     path('vaxpage/', views.vaxpage, name="vaxpage"),
-    # path("vaxpage", views.vaxpage, name="vaxpage"),
-    # path("vaxpage.html", views.vaxpage, name="vaxpage"),
     path('regpage/', views.regpage, name="regpage"),
-    # path("regpage", views.regpage, name="regpage"),
-    # path("regpage.html", views.regpage, name="regpage")
 ]
