58c23c21dc2d4423790d872891722e180ce4e20d
Adam Barker <adambarker@Adams-MBP-New.broadband>
2021-01-17 21:50:56 +0000

    Tidy following merge

--- hello/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class vaxitrackConfig(AppConfig):
-    name = 'vaxitrack'
--- hello/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class BuildForm(forms.Form):
-
-    email = forms.EmailField(label='Email address',
-                help_text="Login code sent via email; NHS.net required", required=True)
-    pc = forms.CharField(label='Postcode',
-                required=True, strip=True)
