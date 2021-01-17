abbec450615a0aebfeb53c13490e73688b95c1f9
tomfrankkirk <tomfrankkirk@gmail.com>
2021-01-17 20:42:07 +0000

    hide secrets

--- vaxitracksite/settings.py
+++ vaxitracksite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3m6r79yi0wfc$(0y_)%l1(6b6ou5b3#gi)$hyko7$ckxe&x)ib'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.path.exists('keys.json'):
+    keys = json.load(open('keys.json', 'r'))
+    SECRET_KEY = keys['SECRET_KEY']
+    DEBUG = True
+else: 
+    SECRET_KEY = os.environ['SECRET_KEY']
+    DEBUG = False
+
 
 ALLOWED_HOSTS = []
 
