fedd0d42e8a6da7237c3dc439d4ead530b2bf105
Adam Barker <adambarker@Adams-MBP-New.lan>
2021-01-18 16:27:44 +0000

    bugfix

--- manage.py
+++ manage.py
@@ -8,7 +8,6 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-=======
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -26,7 +25,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
